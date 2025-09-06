4bbd2e7533120e9322c1dae0edf93cf3e373e8ff
Raushan_kumar_jha <jharaushan2002@gmail.com>
2025-09-06 23:19:21 +0530

    added the cors configuration in this

---
+++ src/main/java/com/rk/social_media/config/corsConfig.java
@@ -0,0 +1,20 @@
+package com.rk.social_media.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsConfig implements WebMvcConfigurer {
+    String frontendUrl = "http://localhost:5173";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
